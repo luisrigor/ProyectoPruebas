5b8b572d1a710ffb3e8e351d11c7b2ed019da1dc
rubyaguibece <aguibece21@gmail.com>
2023-03-21 23:36:09 -0500

    se agrega log en controler

--- src/main/java/com/prueba/proyecto/controller/ProyectInitialController.java
+++ src/main/java/com/prueba/proyecto/controller/ProyectInitialController.java
@@ -3,6 +3,7 @@ package com.prueba.proyecto.controller;
 import com.prueba.proyecto.Entity.Persona;
 import com.prueba.proyecto.service.impl.ProyectInitialServiceImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +14,14 @@ import java.util.Optional;
 @RestController
 @RequestMapping(value = "/v1")
 @RequiredArgsConstructor
+@Slf4j
 public class ProyectInitialController {
 
     private final ProyectInitialServiceImpl serviceImpl;
 
     @GetMapping("/prueba")
     public ResponseEntity<Optional<Persona>> prueba(){
-
+        log.info("Ruby por fin");
         return ResponseEntity.ok(serviceImpl.pruebasService());
     }
 }
